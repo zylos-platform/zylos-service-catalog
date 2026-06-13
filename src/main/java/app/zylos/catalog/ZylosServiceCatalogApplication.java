92ef7b362d99b7d0d9266c1323b8e21742e7b08c
Kamesh Chathuranga <kameshchathuranga7@gmail.com>
2026-06-13 22:34:04 +0530

    initialize zylos-service-catalog project with Maven and Spring Boot setup

---
+++ src/main/java/app/zylos/catalog/ZylosServiceCatalogApplication.java
@@ -0,0 +1,13 @@
+package app.zylos.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZylosServiceCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZylosServiceCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/zylos/catalog/ZylosServiceCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package app.zylos.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZylosServiceCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
